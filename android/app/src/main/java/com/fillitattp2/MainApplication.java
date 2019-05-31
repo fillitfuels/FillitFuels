4b56470e9e2f8d10fdeb8c707bff637d09dfc169
itsazhuhere <andrez@uci.edu>
2019-05-31 14:10:58 -0700

    changed user pool again to one with better settings

--- android/app/src/main/java/com/fillitattp2/MainApplication.java
+++ android/app/src/main/java/com/fillitattp2/MainApplication.java
@@ -3,6 +3,7 @@ package com.fillitattp2;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.amazonaws.RNAWSCognitoPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNAWSCognitoPackage(),
             new VectorIconsPackage(),
             new MapsPackage(),
             new RNGestureHandlerPackage()
--- src/util/config.js
+++ src/util/config.js
@@ -2,10 +2,10 @@
 
 
 const awsConfig = {
-    identityPoolId: 'us-east-2_RvBSPqUqd',
+    identityPoolId: 'us-east-2_W2Mp3tVLv',
     region: 'us-east-2',
-    userPoolId: 'us-east-2_RvBSPqUqd',
-    userPoolWebClientId: '61s9cq1sjnbjmp20iolj3qct7b',
+    userPoolId: 'us-east-2_W2Mp3tVLv',
+    userPoolWebClientId: 's8gn4dl2ddrdaoeovcibdndqg',
 };
 
 export default awsConfig;
