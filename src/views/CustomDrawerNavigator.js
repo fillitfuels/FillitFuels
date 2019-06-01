158a01b9e54927b1d9b243729d36d23a6117b8d1
frank <frankvu25@gmail.com>
2019-06-01 13:34:56 -0700

    Drawer Navigation filler

--- src/views/CustomDrawerNavigator.js
+++ src/views/CustomDrawerNavigator.js
@@ -1,21 +1,26 @@
 import React from 'react';
 
-import {View} from 'react-native';
+import {SafeAreaView, View, ScrollView, Image} from 'react-native';
 import { DrawerItems } from 'react-navigation';
 
 const CustomDrawerNavigator = props => (
     <View style={styles.container}>
+        <View style={{height: 150,backgroundColor: 'white', alignItems: 'center', justifyContent: 'center'}}>
+            <Image source={require('../../assets/Pictures/tesla.png')} style={{height: 120, width:120, borderRadius: 60}} />
+        </View>
+        <ScrollView>
         <DrawerItems
             activeBackgroundColor={"black"}
             activeTintColor={"white"}
             //iconContainerStyle={styles.icons}
             {...props}
         />
+        </ScrollView>
     </View>
 );
 
 
-const styles = {
+const styles = {  //put styles.container in style= if doesnt work
     container: {
         flex: 1
     },
